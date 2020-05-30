8bf0c539ccc8a9655369818582e492009c5617f4
ishantsolanki <ishant360@gmail.com>
2020-05-30 18:57:20 +0100

    Fix tests for login

--- src/components/LoginForm/LoginForm.test.tsx
+++ src/components/LoginForm/LoginForm.test.tsx
@@ -1,19 +1,33 @@
 import React from 'react';
 import { fireEvent } from '@testing-library/react'
 import { renderWithRouter } from '../../testUtils'
-import LoginForm from './LoginForm';
+import { LoginForm } from './LoginForm';
+import { act } from 'react-dom/test-utils';
 
 test('renders Login', () => {
-  const { getByText } = renderWithRouter(<LoginForm />)
+  const { getByText } = renderWithRouter(<LoginForm checkUserBound={() => Promise.resolve() } />)
   const headerElement = getByText(/Login/i)
   expect(headerElement).toBeInTheDocument()
 });
 
-test('shows error when login details incorrect', () => {
-  const { getByLabelText, getByText } = renderWithRouter(<LoginForm />)
-  fireEvent.change(getByLabelText('Username'), {target: {value: 'foo'}})
-  fireEvent.change(getByLabelText('Password'), {target: {value: 'bar'}})
-  fireEvent.click(getByText('Log In'))
+test('shows error when login details incorrect', async () => {
+  const { getByLabelText, getByRole, getByText } = renderWithRouter(<LoginForm checkUserBound={() => Promise.resolve({ result: false }) } />)
 
-  expect(getByText('Invalid username or password')).toBeInTheDocument()
+    await act(async () => {
+      fireEvent.change(getByLabelText('Username'), {target: {value: 'foo'}})
+      fireEvent.change(getByLabelText('Password'), {target: {value: 'bar'}})
+      await fireEvent.click(getByRole('button'))
+      expect(getByText('Invalid username or password')).toBeInTheDocument()
+    })
+})
+
+test('navigates to listing page when login is correct', async () => {
+  const { getByLabelText, getByRole, history } = renderWithRouter(<LoginForm checkUserBound={() => Promise.resolve({ result: true }) } />)
+
+    await act(async () => {
+      fireEvent.change(getByLabelText('Username'), {target: {value: 'foo'}})
+      fireEvent.change(getByLabelText('Password'), {target: {value: 'bar'}})
+      await fireEvent.click(getByRole('button'))
+      expect(history.location.pathname).toEqual('/listings')
+    })
 })
